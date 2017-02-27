c815fd51870a961615aa1d6bb88525d7662bd834
wding <rtb-team@groupon.com>
2017-02-27 15:11:11 -0800

    Add question of "Search for words"

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        new SearchForWords().test();
-
-        /*
-        new RodCutting().TestRodCut();
-        TreeBalance.TestTreeBalance();
-        MatrixFind.test();
-        GetChange.TestGetChange();
-        NSquareQueen ns = new NSquareQueen();
-        ns.PlaceQueen(8);
-        */
+        new StringCutting().test();
+        //new SearchForWords().test();
+        //new RodCutting().TestRodCut();
+        //TreeBalance.TestTreeBalance();
+        //MatrixFind.test();
+        //GetChange.TestGetChange();
+        //new NSquareQueen().PlaceQueen(8);
     }
 }
---
+++ src/StringCutting.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+/**
+ * Created by wding on 2/26/17.
+ */
+public class StringCutting {
+
+    /* == problem ===
+    A certain string-processing language offers a primitive operation which splits a string into two pieces.
+    Since this operation involves copying the original string, it takes n units of time for a string of length n,
+    regardless of the location of the cut. Suppose, now, that you want to break a string into many pieces.
+    The order in which the breaks are made can affect the total running time. For example,
+    if you want to cut a 20-character string at positions 3 and 10, then making the first cut at position 3 incurs
+    a total cost of 20+17=37, while doing position 10 first has a better cost of 20+10=30.
+     */
+    // int[] cuttingPoints has all the cutting position of a string - e.g., 2, 7, 9, 11, ...
+    // strLen is the length of the initial string
+    // Write a function to return the minimun cutting cost
+    private int CutString(int[] cuttingPoints, int st, int end, int strLen){
+        if(st + 1 >= end){ // because there is no cutting point
+            return 0;
+        }
+        if(st + 2 >= end){ // because there is only one cutting point
+            return strLen;
+        }
+
+        // dynamic programming
+        int minVal = Integer.MAX_VALUE;
+        for(int i=st+1; i<end; i++){
+            int leftHalfLen = cuttingPoints[i]-cuttingPoints[st];
+            minVal = Math.min(minVal, strLen + CutString(cuttingPoints, st, i, leftHalfLen) + CutString(cuttingPoints, i, end, strLen - leftHalfLen));
+        }
+        return minVal;
+    }
+
+    public void test(){
+        int strLen = 20;
+        int[] cuttingPoints = {2, 7, 9};
+
+        int[] extendedCuttingPoints = new int[cuttingPoints.length + 2];
+        for(int i=0;i<cuttingPoints.length;i++){ // Note : added 0 to the start and end --> {0,2,7,9,0}
+            extendedCuttingPoints[i+1] = cuttingPoints[i];
+        }
+        System.out.println("The min cutting cost is " + CutString(extendedCuttingPoints, 0, cuttingPoints.length - 1, strLen));
+    }
+}

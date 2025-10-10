6f71c57f8541a83e495b99fae22c3b570514f474
Wen Ding <dingwn@hotmail.com>
2025-10-09 20:10:51 -0700

    Update folder_file_size.py

--- Python/trees/folder_file_size.py
+++ Python/trees/folder_file_size.py
@@ -4,35 +4,35 @@ A folder can have files and sub-folders
 write a function to return file size for a given folder
 
 # get all sizes for all folders
+
+root(d)
+  f1
+  f2
+  f3
+  sf1(d)
+     f1
+     f2
+     f3
+     sf2(d)
 """
+class item:
+    def __init__(self, name, size=0):
+        self.name, self.size = name, size
 
+class folder:
+    def __init__(self, files=[], sub_folders=[]):
+        self.files = files
+        self.sub_folders = sub_folders
 
-class FileSystem:
-    def __init__(self, name, size=0, isFolder=False):
-        self.name, self.size, self.isFolder, self.sub_folders = name, size, \
-                                                                isFolder, []
+def get_all_sizes(fd):
+    total_size = 0
+    total_size += sum([f.size for f in fd.files])
+    for s_f in fd.sub_folders:
+        total_size += get_all_sizes(s_f)
+    return total_size
 
+f1 = folder(files=[item("file1", 10), item("file1", 10)])
+f2 = folder(files=[item("file2", 3), item("file2", 3)])
+r = folder(files=[item("file2", 2), item("file2", 3)], sub_folders=[f1, f2])
 
-def get_all_sizes(node):
-    if not node.isFolder:
-        return node.size
-    if len(node.sub_folders) == 0:
-        return 0
-
-    node.size = sum(get_all_sizes(item) for item in node.sub_folders)
-    return node.size
-
-
-root = FileSystem("root", isFolder=True)
-root.sub_folders.append(FileSystem("file1", 10))
-root.sub_folders.append(FileSystem("file2", 10))
-root.sub_folders.append(FileSystem("file3", 10))
-sub_folder1 = FileSystem("sub_folder1", isFolder=True)
-sub_folder1.sub_folders.append(FileSystem("file1", 5))
-sub_folder1.sub_folders.append(FileSystem("file2", 5))
-sub_folder1.sub_folders.append(FileSystem("file3", 3))
-sub_folder2 = FileSystem("sub_folder2", isFolder=True)
-root.sub_folders.append(sub_folder1)
-root.sub_folders.append(sub_folder2)
-
-print(get_all_sizes(root))
+print(get_all_sizes(r))

58f1e83e39ec1038bfad88c072c28a29ccb2db5d
Wen Ding <dingwn@hotmail.com>
2025-10-18 14:14:13 -0700

    Update Median_2_sorted_arrays.py

--- Python/int_array/Median_2_sorted_arrays.py
+++ Python/int_array/Median_2_sorted_arrays.py
@@ -4,29 +4,40 @@
 #   i1            j1 --> m1 = (i1 + j1) / 2 (inclusive)
 #  [6, 7, 8, 9, 10]
 #   i2            j2 --> m2 = (i2 + j2) / 2 (inclusive)
-# if a[m1] > b[m2] --> first half of a, second half of b
-# base case:
-# [1,2]
-# [3,4]
-# median --> (2+3) / 2
-#  let's assume both array is the same length
-#
+# because median is the element that is > (m + n) // 2 elements for the combined sorted array
+# so method is to merging 2 sorted arrays and when count == (m + n) //2, return that element
 
-def get_median2(a, i1, j1, b, i2, j2):
-    if i1 == j1 and i2 == j2:
-        return (a[i1] + b[i2]) /2
-    if i1 + 1 == j1 and i2 + 1 == j2:
-        return (max(a[i1], b[i2]) + min(a[j1], b[j2])) / 2
+def get_median(a, b):
+    m, n= len(a), len(b)
+    m_idx = (m+n)//2
+    i, j, cnt = 0, 0, 0
 
-    m1 = (i1 + j1) // 2
-    m2 = (i2 + j2) // 2
-    if a[m1] < b[m2]:
-        return get_median2(a, m1, j1, b, i2, m2)
-    else:
-        return get_median2(a, i1, m1, b, m2, j2)
+    while (i < m and j < n):
+        if a[i] < b[j]:
+            if cnt == m_idx:
+                return a[i]
+            i += 1
+        else:
+            if cnt == m_idx:
+                return b[j]
+            j += 1
+        cnt += 1
 
+    while (i < m):
+        if cnt == m_idx:
+            return a[i]
+        i += 1
+        cnt += 1
 
-a1 = [1, 3, 5, 7, 9]
+    while (j < n):
+        if cnt == m_idx:
+            return b[j]
+        j += 1
+        cnt += 1
+    return None
+
+a1 = [1, 3]
 a2 = [2, 4, 6, 8, 10]
 
-print(get_median2(a1, 0, len(a1)-1, a2, 0, len(a2)-1))
+
+print(get_median(a1,a2))

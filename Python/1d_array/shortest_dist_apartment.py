7ed2fd8a7ba372670312de906763947ec54552f8
Wen Ding <dingwn@hotmail.com>
2025-10-19 14:22:53 -0700

    Update shortest_dist_apartment.py

--- Python/1d_array/shortest_dist_apartment.py
+++ Python/1d_array/shortest_dist_apartment.py
@@ -3,85 +3,44 @@
 # for example:
 """
 input_blocks = [
-{"gym":False, "grocery_store":True, "office":False},
-{"gym":False, "grocery_store":True, "office":False},
-{"gym":True, "grocery_store":False, "office":False},
-{"gym":False, "grocery_store":True, "office":True},
-{"gym":False, "grocery_store":False, "office":True},
-{"gym":True, "grocery_store":False, "office":False}
+{"gym":False, "grocery_store":True, "office":False}, # res [(F,0), (T,0),(F,0)] (T/F, index)
+{"gym":False, "grocery_store":True, "office":False}, # res [(F,0), (T,1),(F,0)]
+{"gym":False, "grocery_store":False, "office":False}, # res [(F,0), (T,1),(F,0)]
+{"gym":True, "grocery_store":False, "office":True},   # res [(T,3), (T,1),(T,3)] -> dist = 2
+{"gym":True, "grocery_store":False, "office":True},   # res [(T,4), (T,1),(T,4)]
+{"gym":False, "grocery_store":True, "office":True},   # res [(T,4), (T,5),(T,5)] -> dis = 1
+{"gym":True, "grocery_store":False, "office":False}    # res [(T,6), (T,5),(T,5)]
 ]
-block_idx: 0 -- 1 -- 2 -- 3 -- 4 -- 5
-Solution 1
--- search range x from 1 to len(a)-1 --> [0,1,2,3,4,5] --> [(0,1),(1,2),(2,3),(3,4),(4,5)] --> [(0,1,2),(1,2,3),(2,3,4),(3,4,5)]--> [(0,1,2,3,4),(1,2,3,4,5)]
--- final distance = x / 2, so why not use search range in odd number: 1, 3, 5
-
-Assumption: the following 2 cases has the same distance value as 1
-case 1
-{"gym":True, "grocery_store":False, "office":False}, <-- live here
-{"gym":False, "grocery_store":True, "office":True},
-case 2
-{"gym":False, "grocery_store":False, "office":True},
-{"gym":True, "grocery_store":False, "office":False},  <-- live here
-{"gym":False, "grocery_store":True, "office":False},
-
-block_idx: 0 -- 1 -- 2 -- 3 -- 4 -- 5
-search_range = min(len(blocks)-1, cur_min_distance)
-
-Solution 2
-             (0, 1, 0), (1, 0, 0), (1, 0, 0), (0, 1, 1), (0, 0, 1), (1, 1, 1)
-
-             -1,0,-1    1,0,-1      2,0,-1,   2,3,3      2,3,4       5,5,5
-
-             0,-1       0,1        0,2         0,2
-             1,0        1,0        1,0         1,3
-             2,-1       2,-1       2,-1        2,3
-             1,0        1,0        3,0         3,0
-
-             (0, 1, 0), (1, 0, 0), (0, 0, 0), (1, 0, 1), (0, 0, 1), (1, 1, 0)
-
-             0,-1       0,1        0,1         0,3       0,3         0,5
-             1,0        1,0        1,0         1,0       1,0         1,5
-             2,-1       2,-1       2,-1        2,3       2,4         2,4
 """
-import copy
 import sys
 
-# solution 1
-def find_block_solution1(blocks):
-    for search_range in range(1,len(blocks),2):
-        idx = search_block(blocks, search_range)
-        if idx >= 0:
-            return idx + int(search_range / 2), int(search_range / 2), blocks[idx:idx+search_range]
-        elif idx == -2:
-            return -1, -1,[]
+# linear solution
+def find_block_solution(blks):
+    res = [0] * len(blks[0]) # last index of that attribute = True
+    attrbutes = [False] * len(blks[0])
+    found, cnt = False, sys.maxsize
+    for i, v in enumerate(blks):
+        if all(v): # if all attributes are True in one block
+            return True, 0
+        if any(v) == False:
+            continue
+        for idx, attr in enumerate(v):
+            if attr:
+                res[idx] = i
+                attrbutes[idx] = True
+        if all(attrbutes):
+            found = True
+            pos_min, pos_max = sys.maxsize, 0
+            for idx in res:
+                pos_min = min(pos_min, idx)
+                pos_max = max(pos_max, idx)
+            # print(f"res={res}, pos_min={pos_min}, pos_max={pos_max}")
+            cnt = min(cnt, pos_max - pos_min)
+
+    return found, cnt
 
 
-def search_block(blocks, search_range):
-    size = len(blocks) - 1 if search_range >= len(blocks) else search_range
-    for i in range(len(blocks) - size + 1):
-        if check_block(blocks[i:i+size]):
-            return i
-    return -2 if size == len(blocks) - 1 else -1
-
-
-def check_block(blocks):
-    res = [False for _ in range(len(blocks[0]))]
-    for i in range(len(blocks[0])):
-        for j in range(len(blocks)):
-            res[i] = res[i] or blocks[j][i]
-    return all(res)
-
-
-input_blocks = [
-{"gym":False, "grocery_store":True, "office":False},
-{"gym":False, "grocery_store":False, "office":False},
-{"gym":True, "grocery_store":False, "office":True},
-{"gym":True, "grocery_store":False, "office":True},
-{"gym":False, "grocery_store":False, "office":True},
-{"gym":True, "grocery_store":False, "office":False}
-]
-
-# solution 1
+# solution
 input_blocks = [
 [False, True, False],
 [False, False, False],
@@ -95,5 +54,5 @@ input_blocks = [
 [False, True, False]
 ]
 
-idx, n, res = find_block_solution1(input_blocks)
-print(f"solution1 -- min_dist: {n},  idx:{idx}, res: {res}")
+found, n = find_block_solution(input_blocks)
+print(f"min_dist: {n},  found: {found}")
